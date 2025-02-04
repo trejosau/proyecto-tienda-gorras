9a1bbbdb13777dc88acb5f9fa3fb31944aabe1dc
thenotoriousxul <saulsanchezlopez999@gmail.com>
2025-02-04 11:32:14 -0600

    si

--- frontend/src/services/axiosClient.ts
+++ frontend/src/services/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:7755/api/",
+    baseURL: "http://localhost:5000/api/",
     headers: {
         "Content-Type": "application/json",
     },
